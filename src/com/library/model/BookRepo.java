60f16afdf839855b0aa1ae4f2184b964d977df40
didaryalcin <ddrylcn@gmail.com>
2024-02-03 22:21:38 +0300

    3

--- src/com/library/model/BookRepo.java
+++ src/com/library/model/BookRepo.java
@@ -38,14 +38,7 @@ public class BookRepo implements LibraryBookService {
         return data.getBooksByAuthor(authorName);
     }
 
-    @Override
-    public void updateBook(Book book){
-        if(data.getBooksByTitle(book.getTitle()).equals(book.getTitle())){
-            data.updateBook(book);
-            System.out.println("Book information updated.");
-        }else {
-            System.out.println("Book not found.");
-        }
+
     }
 
     @Override
