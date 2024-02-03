054aa0dd76fba82ba1ea1aae5f5c7252fd917a12
didaryalcin <ddrylcn@gmail.com>
2024-02-03 22:23:56 +0300

    3

--- src/com/library/model/User.java
+++ src/com/library/model/User.java
@@ -72,13 +72,7 @@ public class User {
         this.borrowedBooks = borrowedBooks;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return userId == user.userId && Objects.equals(email, user.email);
-    }
+
 
     @Override
     public int hashCode() {
